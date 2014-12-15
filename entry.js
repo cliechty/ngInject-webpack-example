f4ca85474d2599361cee533baa923c39d4c12f88
Chris Liechty <evilhomer78@gmail.com>
2014-12-14 23:04:30 -0700

    example project

---
+++ entry.js
@@ -0,0 +1,9 @@
+require('angular');
+require('angular-ui-router');
+
+angular.module('app', ['ui-router'])
+
+/* @ngInject */
+function myFunc($stateProvider) {}
+
+var myFunc = /* @ngInject */ function($stateProvider) {};
